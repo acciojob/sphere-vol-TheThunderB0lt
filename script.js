f2b62033cdc9136d02544db04e0e80b8ef312902
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-03-26 20:25:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
-  
+	event.preventDefault();
+	const value = document.getElementById("radius").value;
+	const ans = document.getElementById("volume");
+	if (isNaN(value) || value<0) {
+		ans.value = NaN;
+	}
+	const v = (4/3)*Math.PI*Math.pow(value, 3);
+	ans.value = v.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
