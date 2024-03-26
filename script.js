a0b7a10f25f11b337e823f053cefbc32148b9207
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-03-26 20:28:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function volume_sphere(event) {
-    //Write your code here
 	event.preventDefault();
 	const value = document.getElementById("radius").value;
 	const ans = document.getElementById("volume");
-	if (isNaN(value) || value<0) {
+	if (isNaN(value) || value < 0) {
 		ans.value = NaN;
 	}
-	const v = (4/3)*Math.PI*Math.pow(value, 3);
+	const v = (4 / 3) * Math.PI * Math.pow(value, 3);
 	ans.value = v.toFixed(4);
 } 
 
